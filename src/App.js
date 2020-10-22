017626725f16a12168c7d1cbf244889cc8d41c1d
Alexander Alzate <alex.alzate95@gmail.com>
2020-10-22 01:17:06 -0500

    Estrcutura base del proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home'
+import News from './views/News'
+import DevelopmentTeam from './views/DevelopmentTeam'
+import AboutUs from './views/AboutUs'
+import Header from './components/Header'
+import MyFooter from './components/MyFooter'
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Router>
+        <div className="NavegationBar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            
+            <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/developmentTeam">DevelopmentTeam</Link>
+            </li>
+            <li>
+              <Link to="/aboutUs">AboutUs</Link>
+            </li>
+          </ul>
+
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/news">
+            <News />
+          </Route>
+          <Route exact path="/developmentTeam">
+            <DevelopmentTeam />
+          </Route>
+          <Route exact path="/aboutUs">
+            <AboutUs />
+          </Route>
+        </Switch>
+
+    </Router>
+    <footer>
+      <MyFooter/>
+    </footer>
     </div>
   );
 }
---
+++ src/components/MyFooter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyFooter(){
+        return(
+            <div className="MyFooter">
+                <h1>Footer</h1>
+            </div>
+        )
+}
