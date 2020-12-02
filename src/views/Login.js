89a0e51241c5c7ebe88890c44962eaa75c55d19d
Aalzate95 <alex.alzate95@gmail.com>
2020-12-02 00:40:33 -0500

    Consola admin creada y view login

---
+++ src/views/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class Login extends React.Component{
+    render(){
+        return(
+            <div className="Login">
+                <div className="LoginCard">
+                    <h1>Login</h1>
+                        <input type="text" name="username" placeholder="Username" required></input>
+                        <input type="password" name="password" placeholder="Password" required></input>
+                        <input type="submit"></input>
+                </div>
+            </div>
+            )
+    }
+}
