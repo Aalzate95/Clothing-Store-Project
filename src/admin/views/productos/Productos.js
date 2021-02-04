37700450b6472d77f1231602042e8037c0e0d75f
Aalzate95 <alex.alzate95@gmail.com>
2021-02-04 14:11:18 -0500

    productos admin

--- src/admin/views/productos/Productos.js
+++ src/admin/views/productos/Productos.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 import Dashboard from '../../components/dashboard/Dashboard'
 import {fetchproductolista} from '../../../api/Api'
-
+import './Productos.css'
 const Productos = () => {
     const [Data, setData] = useState({})
 
@@ -15,18 +15,40 @@ const Productos = () => {
         loadData()
     ),[])
 
+    const renderElementos = Object.keys(Data).map((id, index) => {
+        let contenido = Data[id];
+        return(
+            <li key={id} className="table-row">
+                <div className="col1"><input type="checkbox"></input></div>
+                <div className="col2" data-label={contenido.product_name}>{contenido.product_name}</div>
+                <div className="col3" data-label={contenido.product_fecha}>{contenido.product_fecha}</div>
+                <div className="col4" data-label={contenido.product_gender}>{contenido.product_gender}</div>
+                <div className="col5" data-label={contenido.product_price}>{contenido.product_price}</div>
+                <div className="col6" data-label={contenido.product_descount}>{contenido.product_descount}</div>
+                <div className="col7" data-label={contenido.product_gender}>{contenido.product_gender}</div>
+                <div className="col8" ><button className="btn" type="button">Edit</button></div>
+                <div className="col9" ><button className="btn" type="button">X</button></div>
+            </li>
+            )
+    })
+
     return ( 
         <Dashboard>
             <div className="Productos">
-                <div className="tableTitle">
-                    <p></p>
-                    <p>Nombre</p>
-                    <p>Fecha</p>
-                    <p>Genero</p>
-                    <p>Precio</p>
-                    <p>Descuento</p>
-                    <p>Talla</p>
-                </div>
+                <ul className="EstiloTabla">
+                    <li className="table-header">
+                        <div className="col1"><input type="checkbox"></input></div>
+                        <div className="col2">Nombre</div>
+                        <div className="col3">Fecha</div>
+                        <div className="col4">Genero</div>
+                        <div className="col5">Precio</div>
+                        <div className="col6">Descuento</div>
+                        <div className="col7">Talla</div>
+                        <div className="col8">Editar</div>
+                        <div className="col9">Eliminar</div>
+                    </li>
+                    {renderElementos}
+                </ul>
             </div>
         </Dashboard>
         );
